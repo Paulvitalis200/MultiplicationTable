c293e645077f7fc689e919b3840fa0465667db43
Paul Otieno <vitalispaul48@live.com>
2025-11-30 02:23:58 +0300

    Add logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+// Multiplication Table
+// Display the multiplication table of any number entered by the user (from 1 to 10).
+
+/*
+ * 1. Ask user for input
+ * 2. Store input in variable
+ * 3. Loop from 1 - 10 and display table
+ *
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int inputNumber = 0;
+
+    while (true) {
+        std::cout << "Enter a valid number: ";
+        if (!(std::cin >> inputNumber)) {
+            std::cin.clear();
+            std::cin.ignore();
+            continue;
+        }
+
+        break;
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        std::cout << i << 'x' << inputNumber << '=' << inputNumber * i << std::endl;
+    }
+
     return 0;
 }
